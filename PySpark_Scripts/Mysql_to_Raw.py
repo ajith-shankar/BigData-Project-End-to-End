9f1e3c2d646796dab574a70e1f4d5de25c452984
ajith-shankar <ajiths0098@gmail.com>
2023-07-19 16:33:19 +0530

    fixed jdbc url

--- PySpark_Scripts/Mysql_to_Raw.py
+++ PySpark_Scripts/Mysql_to_Raw.py
@@ -3,13 +3,14 @@
 from pyspark.sql import SparkSession
 
 # SparkSession is already created by the Pyspark shell 
-spark = SparkSession.builder.appName("Testing Spark") \
+spark = SparkSession.builder.appName("MySql to Raw") \
     .master("yarn") \
     .getOrCreate()
 
+
 # edit the variables
-conn_url = "jdbc:mysql://localhost/3306/cars"
-conn_driver = "com.mysql.cj.jdbc.driver"
+conn_url = "jdbc:mysql://localhost:3306/cars"
+conn_driver = "com.mysql.cj.jdbc.Driver"
 db_table = "employees"
 db_username = "root"
 db_password = "test"
