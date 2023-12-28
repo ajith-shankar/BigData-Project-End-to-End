53b501f3e180a13030470aa38084854370068f51
Ajith <ajiths0098@gmail.com>
2023-12-28 14:36:45 +0530

    folder structure

---
+++ src/main/python/bin/run_presc_pipeline.py
@@ -0,0 +1,35 @@
+#import all the necessary modules
+
+def main():
+    # get all the variables
+
+    # get Spark objects
+        # validate spark object
+        # setup logging mechanism
+        # setup error handling mechanism
+
+    # initiate run_presc_data_ingest script
+        # load the City File
+        # load the Prescriber Fact file
+        # validate
+        # Setup logging config mechanism
+        # setup Error handling mechanism
+
+    # initiate run_presc_data_preprocessing script
+        # perform data cleaning operations
+        # validate
+        # Setup logging config mechanism
+        # setup Error handling mechanism  
+
+    # initiate run_presc_data_transform script
+        # apply all the transformation logics
+        # validate
+        # Setup logging config mechanism
+        # setup Error handling mechanism    
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
