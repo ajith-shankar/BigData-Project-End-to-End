0e6c23affd3a779450a3411135c19c73aec69b9c
Ajith <ajiths0098@gmail.com>
2023-12-28 22:54:50 +0530

    added few py scripts

---
+++ src/main/python/bin/get_all_variables.py
@@ -0,0 +1,20 @@
+import os
+
+# set environment variables
+os.environ['envn'] = 'Test'
+os.environ['header'] = 'True'
+os.environ['inferSchema'] = 'True'
+
+# Get environment variables
+envn = os.environ['envn']
+header = os.environ['header']
+inferSchema = os.environ['inferSchema']
+
+# Set other variables
+appName = "USA Prescriber Report"
+currentPath = os.getcwd()
+staging_dim = currentPath + '/staging/dim'
+staging_fact = currentPath + '/staging/fact'  # to escape \f
+
+
+
--- src/main/python/bin/run_presc_pipeline.py
+++ src/main/python/bin/run_presc_pipeline.py
@@ -1,36 +1,37 @@
 # import all the necessary modules
+import get_all_variables as gav
+from create_objects import get_spark_object
 
 
 def main():
     # get all the variables
+    spark = get_spark_object(gav.envn, gav.appName)
+    print("Spark object is created...")
 
     # get Spark objects
-        # validate spark object
-        # setup logging mechanism
-        # setup error handling mechanism
+    # validate spark object
+    # setup logging mechanism
+    # setup error handling mechanism
 
     # initiate run_presc_data_ingest script
-        # load the City File
-        # load the Prescriber Fact file
-        # validate
-        # Setup logging config mechanism
-        # setup Error handling mechanism
+    # load the City File
+    # load the Prescriber Fact file
+    # validate
+    # Setup logging config mechanism
+    # setup Error handling mechanism
 
     # initiate run_presc_data_preprocessing script
-        # perform data cleaning operations
-        # validate
-        # Setup logging config mechanism
-        # setup Error handling mechanism  
+    # perform data cleaning operations
+    # validate
+    # Setup logging config mechanism
+    # setup Error handling mechanism
 
     # initiate run_presc_data_transform script
-        # apply all the transformation logics
-        # validate
-        # Setup logging config mechanism
-        # setup Error handling mechanism    
-    pass
+    # apply all the transformation logics
+    # validate
+    # Setup logging config mechanism
+    # setup Error handling mechanism
 
 
 if __name__ == "__main__":
     main()
-
-
